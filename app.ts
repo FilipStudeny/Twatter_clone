6445eec87a9344a918d54cdb6712703f53002d8e
Filip Studený <BluesyPompanno@seznam.cz>
2022-10-28 18:00:03 +0200

    Registrace, Login a ověření formuláře

--- app.ts
+++ app.ts
@@ -3,6 +3,7 @@ import * as dotenv from 'dotenv'
 import { requireLogin} from './middleware/authentication';
 import { route as loginRoutes } from './routes/loginRoutes';
 import path from 'path';
+import bodyParser from "body-parser";
 
 // *** CONFIG *** //
 dotenv.config();
@@ -11,13 +12,17 @@ const PORT: any = process.env.PORT;
 const app: Application = express();
 
 // *** MIDDLEWARE AND STUFF *** //
+app.use(bodyParser.urlencoded({ extended: false }));
+
+//app.use(bodyParser.json()); //NEEDS TO RECEIVE DATA IN JSON FORMAT 
+
 app.set('view engine', 'pug')
 app.set('views', 'views')
 app.use(express.static(path.join(__dirname, 'public'))) //CSS from public
 
 
 // *** ROUTES *** //
-app.use('/login', loginRoutes)
+app.use('/', loginRoutes)
 
 app.get('/', requireLogin, (req: Request, res: Response, next: NextFunction) => {
 
--- routes/loginRoutes.ts
+++ routes/loginRoutes.ts
@@ -5,7 +5,7 @@ export const route = express.Router();
 
 
 // *** ROUTES *** //
-route.get('/',  (req: Request, res: Response, next: NextFunction) => {
+route.get('/login',  (req: Request, res: Response, next: NextFunction) => {
     const payload: Object = {
         pageTitle : "Login page"
     }
@@ -14,4 +14,34 @@ route.get('/',  (req: Request, res: Response, next: NextFunction) => {
     res.status(200)
     res.render('login', payload)
 })
+
+route.get('/register',  (req: Request, res: Response, next: NextFunction) => {
+    const payload: Object = {
+        pageTitle : "Register page"
+    }
+
+
+    res.status(200)
+    res.render('register', payload)
+})
+
+route.post('/register',  (req: Request, res: Response, next: NextFunction) => {
+
+    const firstName = req.body.firstName.trim();
+    const lastName = req.body.lastName.trim();
+    const password = req.body.password.trim();
+    const email = req.body.email.trim();
+    const username = req.body.username;
+
+    const payload = req.body;
+
+    if(firstName && lastName && password && email && username){
+        
+    }else{
+        payload.errorMessage = "Form fields are empty"
+        res.status(200).render('register', payload)
+    }
+
+})
+
 
